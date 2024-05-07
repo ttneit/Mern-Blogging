95c0542c6793e98f1249f1b5f31fc910d9ac3a80
ttneit <thinhtien1312@gmail.com>
2024-05-07 08:23:15 +0700

    fix update user

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -9,7 +9,7 @@ export const updateUser = async(req,res,next) => {
         if (req.body.password.length  < 6  ) {
             return next(errorHandler(400,'Password must be at least 6 characters'))
         }
-        req.body.password = bcryptjs.hashSync(req.params.password,10);
+        req.body.password = bcryptjs.hashSync(req.body.password,10);
     }
     if (req.body.username) {
         if (req.body.username.length  < 7   || req.body.username.length > 20) {
@@ -60,7 +60,7 @@ export const signout = (req,res,next) => {
         res.clearCookie('access_token');
         res.status(200).json('User has been signed out');
     } catch (error) {
-        nextt(error);
+        next(error);
     }
 }
 
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -10,7 +10,7 @@ export default function SignUp() {
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
-  
+  console.log(formData);
   const handleSubmit = async (e) =>{
     e.preventDefault();
     if(!formData.username || !formData.password || !formData.email) {
