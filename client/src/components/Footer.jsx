b9bcd8c38185ea6d5651fcdfb05f1a0de5faba53
ttneit <thinhtien1312@gmail.com>
2024-05-29 21:18:24 +0700

    update footer and config google oauth and change home page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,33 +4,26 @@ import { BsDribbble, BsFacebook, BsGithub, BsInstagram, BsTwitter } from "react-
 import { Link } from 'react-router-dom'
 export default function FooterComponent() {
   return (
-    <Footer container>
+    <Footer container className='border border-t-4 border-teal-500'>
         <div className="w-full">
-          <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
+          <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-2">
             <div>
               <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'> 
                 <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Personal Blogs</span>
               </Link>
             </div>
-            <div>
+            {/* <div>
               <Footer.Title title='About'/>
               <Footer.LinkGroup col>
                 <Footer.Link href='#' >Other projects</Footer.Link>
-                <Footer.Link href='#' >Personal project</Footer.Link>
+                <Footer.Link href='#'>Personal project</Footer.Link>
               </Footer.LinkGroup>
-            </div>
+            </div> */}
             <div>
               <Footer.Title title='Connect'/>
               <Footer.LinkGroup col>
-                <Footer.Link href='#' >Github</Footer.Link>
-                <Footer.Link href='#' >Linkedin</Footer.Link>
-              </Footer.LinkGroup>
-            </div>
-            <div>
-              <Footer.Title title='Legal'/>
-              <Footer.LinkGroup col>
-                <Footer.Link href='#' >Privacy</Footer.Link>
-                <Footer.Link href='#' >Terms and Conditions</Footer.Link>
+                <Footer.Link href='https://github.com/ttneit' >Github</Footer.Link>
+                <Footer.Link href='https://www.linkedin.com/in/th%E1%BB%8Bnh-ti%E1%BA%BFn-196408256/' >Linkedin</Footer.Link>
               </Footer.LinkGroup>
             </div>
           </div>
@@ -38,11 +31,8 @@ export default function FooterComponent() {
         <div className="w-full sm:flex sm:items-center sm:justify-between">
         <Footer.Copyright by='Me' year = {new Date().getFullYear()} />
         <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
-          <Footer.Icon href="#" icon={BsFacebook} />
-            <Footer.Icon href="#" icon={BsInstagram} />
-            <Footer.Icon href="#" icon={BsTwitter} />
-            <Footer.Icon href="#" icon={BsGithub} />
-            <Footer.Icon href="#" icon={BsDribbble} />
+          <Footer.Icon href="https://www.facebook.com/thinhtien.lephuoc.9/" icon={BsFacebook} />
+          <Footer.Icon href="https://github.com/ttneit" icon={BsGithub} />
         </div>
         </div>
         </div>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,7 +9,7 @@ export default function Home() {
   useEffect(()=> {
     const fetchPosts = async() => {
       try {
-        const res = await fetch(`/api/post/getposts?limit=5`)
+        const res = await fetch(`/api/post/getposts?limit=6`)
         const data = await res.json();
         setPosts(data.posts);
       } catch (error) {
@@ -30,7 +30,7 @@ export default function Home() {
         <CallToAction />
       </div> */}
 
-      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
+      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-6 py-7'>
         {
           posts && posts.length > 0 && (
             <div className='flex flex-col gap-6'>
