72bf5533fddc883bba6280f9ccd2da651e15289c
ttneit <thinhtien1312@gmail.com>
2024-06-01 16:23:18 +0700

    change

--- server/api/index.js
+++ server/api/index.js
@@ -27,9 +27,11 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.options('*',cors());
 const __dirname =  path.resolve();
-app.use(express.json());
+// app.use(express.json());
+// app.use(cookieParser());
+app.use(express.json({ limit: '50mb' })); // Increase JSON payload limit
+app.use(express.urlencoded({ limit: '50mb', extended: true })); // Increase URL-encoded payload limit
 app.use(cookieParser());
-
 app.listen(3001 , () => {
     console.log('Server is running on port 3001');
 });
